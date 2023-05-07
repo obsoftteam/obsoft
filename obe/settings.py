ec7376442cedf49f429011193fc5b2562429d671
obsoftteam <132460296+obsoftteam@users.noreply.github.com>
2023-05-07 06:25:46 +0530

    Update settings.py

--- obe/settings.py
+++ obe/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 
 # STATIC_URL = 'staticfiles/'
 
-STATIC_URL = 'https://github.com/obsoftteam/obsoft/tree/main/staticfiles'
+STATIC_URL = 'https://github.com/obsoftteam/obsoft/tree/main/staticfiles/'
 
 # STATIC_ROOT = "staticfiles/"
 
